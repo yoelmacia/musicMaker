a3ee6f11ad6e6bf3371cdc0dbefab9ebe10054fe
YoelMacia <yoelmaciadelgado@gmail.com>
2019-08-21 13:17:50 +0200

    Add Bootstrap
    Start making the mookup of the app using bootstrap and
    already differentiating three components for the gallery,
    the title and the profile of each artist.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="Title">Music Maker</div>
+    <div class="Profile">
+      <button>List Songs</button>
+      <div>Artist Picture</div>
+      <div>Artist Name</div>
+    </div>
+    <div class="Gallery">Gallery</div>
+  </div>
 </template>
 
 <script>
@@ -10,4 +18,15 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: black;
+  color: #fff;
+}
+#app {
+  text-align: center;
+  padding: 5%;
+}
+.Title {
+  font-size: 30px;
+}
 </style>
